08b29362a97565d983e711ebb0d4ee9c7fafd30d
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-20 17:39:59 +0100

    add class users

---
+++ src/Entities/user.php
@@ -0,0 +1,12 @@
+<?php
+// src/Entities/user.php
+
+class User {
+    private int $id;
+    private string $nom;
+    private string $prenom;
+    private string $email;
+    private string $password;
+    private int $points;
+    private string $label_role;
+    
