ca8f179c3a242bc5cb7dc1db432385da6f9034ab
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-20 17:41:16 +0100

    add construct des atributs

--- src/Entities/user.php
+++ src/Entities/user.php
@@ -9,4 +9,12 @@ class User {
     private string $password;
     private int $points;
     private string $label_role;
-    
+    public function __construct(int $id, string $nom, string $prenom, string $email, string $password, int $points, string $label_role) {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->password = $password;
+        $this->points = $points;
+        $this->label_role = $label_role;
+    }
