7ab05b65cc85026033b8727a82288dd39c09314c
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-20 17:46:34 +0100

    add construct the class userrepository

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -6,3 +6,8 @@ require_once __DIR__ . '/../Entities/user.php';
 
 class UserRepository {
     private PDO $db;
+    public function __construct() {
+        // N-khdmo b la classe Database li saubti
+        $database = new Database();
+        $this->db = $database->connect();
+    }
