a48fb27e6c16fdd886f5fbd743b83abf719dd859
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-20 17:48:19 +0100

    add function findbyemail for find user

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -11,3 +11,25 @@ class UserRepository {
         $database = new Database();
         $this->db = $database->connect();
     }
+    public function findByEmail(string $email): ?User {
+        $query = "SELECT * FROM users WHERE email = :email LIMIT 1";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([':email' => $email]);
+
+        $row = $stmt->fetch();
+        if (!$row) {
+            return null; // Ila l-email makaynx
+        }
+
+        // N-rj3ou l-m3loumat 3la xkl Object (User Entity)
+        return new User(
+            $row['id'],
+            $row['nom'],
+            $row['prenom'],
+            $row['email'],
+            $row['password'],
+            $row['points'],
+            $row['label_role']
+        );
+    }
+}
