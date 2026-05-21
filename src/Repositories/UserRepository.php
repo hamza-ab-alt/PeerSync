ae3c87729937aac68d964f1ee0b88cf196efaf49
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-21 17:22:42 +0100

    add index php cpde

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -7,9 +7,7 @@ require_once __DIR__ . '/../Entities/user.php';
 class UserRepository {
     private PDO $db;
     public function __construct() {
-        // N-khdmo b la classe Database li saubti
-        $database = new Database();
-        $this->db = $database->connect();
+        $this->db = (new Database())->connect();
     }
     public function findByEmail(string $email): ?User {
         $query = "SELECT * FROM users WHERE email = :email LIMIT 1";
